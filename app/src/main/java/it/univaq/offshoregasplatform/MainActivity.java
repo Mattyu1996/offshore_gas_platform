1fbe49b5660b6b7dde9a5edf04a5f6b33305499a
Cico96 <40207930+Cico96@users.noreply.github.com>
2020-02-10 12:45:58 +0100

    fix

--- app/src/main/java/it/univaq/offshoregasplatform/MainActivity.java
+++ app/src/main/java/it/univaq/offshoregasplatform/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //Test
         super.onCreate(savedInstanceState);
+        //gianfranco
         setContentView(R.layout.activity_main);
     }
 }
