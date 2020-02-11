4acd80b738683b72f9a900a4b4932778792a1552
Cico96 <40207930+Cico96@users.noreply.github.com>
2020-02-11 11:59:05 +0100

    database

---
+++ app/src/main/java/it/univaq/offshoregasplatform/DataBase.java
@@ -0,0 +1,31 @@
+package it.univaq.offshoregasplatform;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+/**
+ * SettimaLezione
+ * Created by leonardo on 2019-11-15.
+ */
+
+@Database(entities = GasPlatform.class, version = 1)
+public abstract class DataBase extends RoomDatabase {
+
+    //public abstract CityDao cityDao();
+
+    private static DataBase instance = null;
+
+    public static DataBase getInstance(Context context) {
+        if(instance == null) {
+            instance = Room.databaseBuilder(
+                    context,
+                    DataBase.class,
+                    "database.db")
+                    .build();
+        }
+        return instance;
+    }
+}
