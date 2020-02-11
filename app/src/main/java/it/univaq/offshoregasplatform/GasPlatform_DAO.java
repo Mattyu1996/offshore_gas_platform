2595d2d7e5fb93b4c60e23ec796aaa17fd541661
Cico96 <40207930+Cico96@users.noreply.github.com>
2020-02-11 13:09:22 +0100

    metodi dao

--- app/src/main/java/it/univaq/offshoregasplatform/GasPlatform_DAO.java
+++ app/src/main/java/it/univaq/offshoregasplatform/GasPlatform_DAO.java
@@ -1,11 +1,16 @@
 package it.univaq.offshoregasplatform;
 
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 
 public interface GasPlatform_DAO {
 
-@Query("SELECT * FROM trivelle ORDER BY denominazione ")
+@Query("SELECT * FROM trivelle ORDER BY denominazione ASC")
     public List<GasPlatform> getAll();
+
+@Insert(onConflict = OnConflictStrategy.REPLACE)
+    public List<Long> save(List<GasPlatform> gasPlatforms);
 }
--- app/src/main/java/it/univaq/offshoregasplatform/MainActivity.java
+++ app/src/main/java/it/univaq/offshoregasplatform/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //gianfranco
         setContentView(R.layout.activity_main);
+        ArrayList g = new ArrayList<GasPlatform>();
+        GasPlatform gs = new GasPlatform();
+        gs.setAltezza(180);
+        gs.setAnnoCostruzione(2020);
+        gs.setCapitaneriaDiPorto("capitaneria");
+        gs.setCentraleCollegata("pes");
+        gs.setCodice(5050);
+        gs.setDenominazione("pasqualone");
+        gs.setDimensioni("tanto");
+        gs.setDistanzaCosta(10);
+        gs.setFoglio("un tot");
+        gs.setLatitudine(55.99);
+        gs.setLongitudine(56.99);
+        gs.setMinerale("quarzo");
+        gs.setOperatore("giggi");
+        gs.setPozziAllacciati(4);
+        gs.setPozziInMonitoraggio(2);
+        gs.setPozziInProduzione(6);
+        gs.setPozziProduttiviNonEroganti(0);
+        gs.setProfonditaFondale(8);
+        gs.setSezioneUnimig("non so di che parli");
+        gs.setStato("italia");
+        gs.setTipo("forza pescara");
+        gs.setTitoloMinerario("tosto");
+        gs.setZona("colliwood");
+
+        DataBase.getInstance(this).gasPlatform_dao().save(g);
+
     }
 }
