7db10522303875297efabe3f34d991291be84902
Mattyu Lenza <mattia.lenza@student.univaq.it>
2020-02-12 13:26:12 +0100

    Calcolo piattaforme vicine

--- app/src/main/java/it/univaq/offshoregasplatform/MainActivity.java
+++ app/src/main/java/it/univaq/offshoregasplatform/MainActivity.java
@@ -1,45 +1,45 @@
 package it.univaq.offshoregasplatform;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
 import java.util.ArrayList;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    FusedLocationProviderClient locationClient;
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
 
             String action = intent.getAction();
             if (action.equals(DatabaseService.FILTER_GET)) {
-                ArrayList<GasPlatform> platforms = intent.getParcelableArrayListExtra("platforms");
                 GasPlatformViewModel provider = ViewModelProviders.of(MainActivity.this).get(GasPlatformViewModel.class);
+                ArrayList<GasPlatform> platforms = intent.getParcelableArrayListExtra("platforms");
                 provider.setPlatforms(platforms);
-                provider.getPlatforms().observe(MainActivity.this, new Observer<ArrayList<GasPlatform>>() {
 
-                    @Override
-                    public void onChanged(ArrayList<GasPlatform> platforms) {
-
-                        System.out.println("Nel ViewModel ci sono: "+platforms.size()+" piattaforme");
-
-                    }
-                });
+                //Aggiorno le piattaforme vicine
+                calculateNearLocations();
             }
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Test
+
         super.onCreate(savedInstanceState);
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
 
         setContentView(R.layout.activity_main);
 
@@ -59,10 +59,11 @@ public class MainActivity extends AppCompatActivity {
 
         LocalBroadcastManager.getInstance(this)
                 .registerReceiver(receiver, new IntentFilter(DatabaseService.FILTER_GET));
-
+        //Chiamo il Database Service per aggiornare la lista delle piattaforme nel provider
         Intent intent = new Intent(MainActivity.this, DatabaseService.class);
         intent.putExtra(DatabaseService.EXTRA_ACTION, DatabaseService.ACTION_GET);
         startService(intent);
+
     }
 
     @Override
@@ -71,5 +72,34 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("pausa");
         LocalBroadcastManager.getInstance(this)
                 .unregisterReceiver(receiver);
+    }
+
+    protected void calculateNearLocations(){
+
+        final GasPlatformViewModel provider = ViewModelProviders.of(MainActivity.this).get(GasPlatformViewModel.class);
+        final ArrayList<GasPlatform> piattaforme = provider.getPlatforms().getValue();
+        System.out.println("Nel ViewModel ci sono: "+piattaforme.size()+" piattaforme");
+        //Ottengo la location dello smartphone
+        locationClient.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                ArrayList<GasPlatform> piattaformeVicine = new ArrayList<>();
+                //Per ogni piattaforma presente nel ViewModel calcolo la distanza dall'utente
+                for (GasPlatform plt: piattaforme){
+                    //Calcolo la distanza fra la location dello smartphone e la piattaforme in questione
+                    float[] results = new float[1];
+                    Location.distanceBetween(location.getLatitude(), location.getLongitude(), plt.getLatitudine(), plt.getLongitudine(), results);
+                    //Converto la distanza da metri a chilometri
+                    float distanzaInKm = results[0]/1000;
+                    //System.out.println("Distanza dalla piattaforma "+plt.getDenominazione()+": "+distanzaInKm+" km");
+                    //Se la piattaforma è nel raggio di 100km allora la inserisco nelle piattaforme vicine
+                    if(distanzaInKm <= 100){
+                        piattaformeVicine.add(plt);
+                    }
+                }
+                System.out.println("Nel ViewModel ci sono: "+piattaformeVicine.size()+" piattaforme vicine");
+                provider.setNearPlatforms(piattaformeVicine);
+            }
+        });
     }
 }
